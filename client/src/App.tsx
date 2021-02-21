ff7f8d8b854d61cfcbf141541cc01e43926df7da
Henry Ryu <hkryucr@gmail.com>
2021-02-20 17:40:46 -0800

    initial commit

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,25 +1,7 @@
 import React from "react";
 
-let initialValue = "hello";
-
 function App() {
-  const he = "ass";
-  const hello = "world";
-
-  return (
-    <div className="App">
-      Frontendasdfsadfasdfasdfasdf asdfas
-      <dfn>
-        asd
-        <fieldset>
-          <dfn>
-            <samp>d</samp>
-          </dfn>
-        </fieldset>
-      </dfn>
-      <span>${initialValue} !11222</span>
-    </div>
-  );
+  return <div className="App">Frontend</div>;
 }
 
 export default App;
