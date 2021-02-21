da17e50a352db90087e638aae07de81b62ba2f3f
Henry Ryu <hkryucr@gmail.com>
2021-02-20 17:27:51 -0800

    initial commit

---
+++ client/src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+let initialValue = "hello"
+
+function App() { 
+  return                   (
+    <div className="App">Frontendasdfsadfasdfasdfasdf
+    asdfas      
+    <dfn>asd   
+      <fieldset><dfn><samp>d</samp></dfn></fieldset>
+    </dfn>
+    <span>${initialValue} !11222</span>
+    </div>
+  );
+}   
+
+export default                       App;
